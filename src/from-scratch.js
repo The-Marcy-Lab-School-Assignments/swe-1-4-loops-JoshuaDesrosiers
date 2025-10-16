fe929b12e4ea9fe0a743fd42ee1a6c55bb7cabd3
JoshuaDesrosiers <desrosiersjoshua3@gmail.com>
2025-10-15 22:56:59 -0400

    completed

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,20 +1,44 @@
 const loop0UpTo10 = () => {
+  for(let i=0;i<10;i++){
+    console.log(i)
+  }
 };
 
 const loop5to10 = () => {
+  for(let i=5;i<=10;i++){
+    console.log(i)
+  }
 };
 
 // you do NOT need an if check
 const loopEvenNumbersUpTo10 = () => {
+  for(let i=0;i<10;i+=2){
+    console.log(i)
+  }
 };
 
 const countdown5to0 = () => {
+  for(let i=5;i>=0;i--){
+    console.log(i)
+  }
 };
 
-const loopUpToNum = () => {
+const loopUpToNum = (num) => {
+  for(let i=0;i<num;i+=1){
+    console.log(i)
+  }
+
 };
 
 const fizzbuzz = () => {
+  for(let i=1;i<=100;i++){
+    i%3==0?
+      i%5==0?
+        console.log('fizzbuzz'):
+    console.log('fizz'):
+    i%5==0?
+      console.log('buzz'):console.log(i)
+  }
 };
 
 module.exports = {
